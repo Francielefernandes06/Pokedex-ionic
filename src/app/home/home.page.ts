d69bfab789ed0c84ea7a747e35cf09943b46a36d
franciele <francielefernandes126@gmail.com>
2023-10-20 13:45:59 -0300

    Bugfix: ajuste nas rotas

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -13,6 +13,9 @@ export class HomePage {
   user: any;
   searchTerm: string | undefined;
   users: any[] | undefined;
+  errorMessage: string | undefined;
+  loading: boolean = false;
+
 
   constructor(private navCtrl: NavController, private http: HttpClient) {
     const usuarioJson = localStorage.getItem('user'); // Recupere o JSON do usuário
@@ -25,26 +28,38 @@ export class HomePage {
   }
 
   searchUsers() {
-
-    const token = localStorage.getItem('token'); 
+    this.loading = true;
+    const token = localStorage.getItem('token');
 
     const headers = new HttpHeaders({
       'Authorization': `Bearer ${token}`
     });
-    this.http.get(`http://127.0.0.1:8000/api/user-by-name?name=${this.searchTerm}`, { headers: headers }).subscribe((data: any) => {
-      this.users = data;
-    });
+    this.http.get(`http://127.0.0.1:8000/api/user-by-name?name=${this.searchTerm}`, { headers: headers }).subscribe(
+      (data: any) => {
+        this.loading = false;
+        if (data.length > 0) {
+          this.users = data;
+        } else {
+          // Não foram encontrados usuários
+          this.users = [];
+          // Defina uma mensagem para exibir
+          this.errorMessage = 'Nenhum usuário encontrado.';
+        }
+      },
+      (error) => {
+        // Lida com erros, se necessário
+        this.loading = false;
+        this.errorMessage = 'Nenhum usuário encontrado.';
+        console.error('Erro na busca de usuários:', error);
+      }
+    );
   }
 
   viewPokemonDetails(user: any) {
     console.log('Dados do usuário:', user);
 
 
-  this.navCtrl.navigateForward('/pokemon-details-user', { state: { user } });
-  }
-
-  irParaTelaLogin() {
-    this.navCtrl.navigateForward('/login');
+    this.navCtrl.navigateForward('/pokemon-details-user', { state: { user } });
   }
 
 }
